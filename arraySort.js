6341817ae8669797dba44ea157da8ddcf971ac61
CameronG <94017817+EdgarAllenBro@users.noreply.github.com>
2021-12-01 08:22:01 -0700

    Add files via upload

---
+++ arraySort.js
@@ -0,0 +1,13 @@
+const numbers = [1, 4, 11, 2, 37, -4]
+
+numbers.sort(function(a,b){return a - b})
+
+let large = numbers [numbers.length -1];
+let small = numbers [0];
+
+console.log ("Array",numbers);
+
+
+console.log ("largest",large);
+console.log ("Smallest",small);
+console.log ("good job")
